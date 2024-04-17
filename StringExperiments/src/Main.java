0fa19dbb717b94d4a96b0d188bd894362c740cbf
GermanGric <gric427@gmail.com>
2024-04-17 14:01:16 +0300

    LocalDate

--- StringExperiments/src/Main.java
+++ StringExperiments/src/Main.java
@@ -1,11 +1,13 @@
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         String name = "Дмитрий";
         String surname = "Петров";
-        int age = 55;
-        String personInfo = name +" " + surname + " - " + age + " лет." ;
+        LocalDate birthday = LocalDate.of(2000,8,8);
+        String personInfo = name +" " + surname + " - " + birthday;
         System.out.println(personInfo);
 
     }
