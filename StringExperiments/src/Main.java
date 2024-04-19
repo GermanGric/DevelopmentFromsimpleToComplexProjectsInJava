5ca566647572d38ca1a94751830574fb0582436e
GermanGric <gric427@gmail.com>
2024-04-19 14:59:48 +0300

    String hello name and surname and Scanner,

--- StringExperiments/src/Main.java
+++ StringExperiments/src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello\nГрицевич Герман" );
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input your first name ");
+        String fname = input.next();
+        System.out.println("Input your last name");
+        String lname = input.next();
+        System.out.println();
+        System.out.println("Hello \n" + fname + " " + lname);
     }
 }
