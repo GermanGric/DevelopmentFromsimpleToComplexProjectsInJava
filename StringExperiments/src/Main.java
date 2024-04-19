d5c0eecf0aaffd50da2642b74f58976ac4aa9022
GermanGric <gric427@gmail.com>
2024-04-19 15:17:13 +0300

    Adding two numbers

--- StringExperiments/src/Main.java
+++ StringExperiments/src/Main.java
@@ -2,12 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Input your first name ");
-        String fname = input.next();
-        System.out.println("Input your last name");
-        String lname = input.next();
-        System.out.println();
-        System.out.println("Hello \n" + fname + " " + lname);
+        int a = 74;
+        int b = 36;
+        System.out.println(a + b);
     }
 }
