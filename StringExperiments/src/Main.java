931b34cd308c49a834484caa6d06434efec69b91
GermanGric <gric427@gmail.com>
2024-04-19 14:51:42 +0300

    String hello name and surname

--- StringExperiments/src/Main.java
+++ StringExperiments/src/Main.java
@@ -1,14 +1,5 @@
-import java.time.LocalDate;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        String name = "Дмитрий";
-        String surname = "Петров";
-        LocalDate birthday = LocalDate.of(2000,8,8);
-        String personInfo = name +" " + surname + " - " + birthday;
-        System.out.println(personInfo);
-
+        System.out.println("Hello\nГрицевич Герман" );
     }
 }
