6043032205cfd733392c1744256831db4156726f
GermanGric <gric427@gmail.com>
2024-04-19 15:41:12 +0300

    Canculator

--- StringExperiments/src/Main.java
+++ StringExperiments/src/Main.java
@@ -7,8 +7,14 @@ public class Main {
         int num1 = input.nextInt();
         System.out.println("input the second number");
         int num2 = input.nextInt();
-        int sum = num1 + num2;
+//        System.out.println("input the third number");
+//        int num3 = input.nextInt();
+        int a = num1 + num2;
+        int b = num1 - num2;
+        int c = num1 * num2;
+        int d = num1 / num2;
         System.out.println();
-        System.out.println("Sum: " + sum);
+        System.out.println(( num1 +" + "+ num2 + " = " + a  ) + "\n" +(num1 +" - "+ num2 + " = " + b) + "\n" +
+        (num1 +" * "+ num2 + " = " + c) +"\n"+ (num1 +" / "+ num2 + " = " + d));
     }
 }
