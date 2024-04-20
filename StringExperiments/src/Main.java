85082083c94f48b0188485976aac0019a197b60e
GermanGric <gric427@gmail.com>
2024-04-20 18:07:25 +0300

    calculation of the area and perimeter of the circle

--- StringExperiments/src/Main.java
+++ StringExperiments/src/Main.java
@@ -4,9 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Input the first number");
-        double r = input.nextDouble();
+        int num1 = input.nextInt();
+        System.out.println("Input the second number");
+        int num2 = input.nextInt();
+        System.out.println("Input the third number");
+        int num3 = input.nextInt();
         System.out.println();
-        System.out.println("Area = " + r * r * Math.PI);
-        System.out.println("Perimeter = " + 2 * r * Math.PI );
+        System.out.println("the average value of the number: " + (num1+num2+num3)/3);
     }
 }
