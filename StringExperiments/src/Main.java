398388b213c8102088149c5eabdc0148b130f50a
GermanGric <gric427@gmail.com>
2024-04-20 18:38:23 +0300

    USA flag

--- StringExperiments/src/Main.java
+++ StringExperiments/src/Main.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Input the first number");
-        int num1 = input.nextInt();
-        System.out.println("Input the second number");
-        int num2 = input.nextInt();
-        System.out.println("Input the third number");
-        int num3 = input.nextInt();
-        System.out.println();
-        System.out.println("the average value of the number : " + (num1+num2+num3)/3);
+        System.out.println("* * * * * * ==================================                          \n" +
+                " * * * * *  ==================================                          \n" +
+                "* * * * * * ==================================                          \n" +
+                " * * * * *  ==================================                          \n" +
+                "==============================================                          \n" +
+                "==============================================                          \n" +
+                "==============================================                          \n" +
+                "==============================================                          \n" +
+                "==============================================                          \n");
     }
 }
