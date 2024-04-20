fb351c276924bef9df8c02c308b7b941337ed9e7
GermanGric <gric427@gmail.com>
2024-04-20 17:04:27 +0300

    The multiplication table

--- StringExperiments/src/Main.java
+++ StringExperiments/src/Main.java
@@ -4,17 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Input the first number");
-        int num1 = input.nextInt();
-        System.out.println("input the second number");
-        int num2 = input.nextInt();
-//        System.out.println("input the third number");
-//        int num3 = input.nextInt();
-        int a = num1 + num2;
-        int b = num1 - num2;
-        int c = num1 * num2;
-        int d = num1 / num2;
-        System.out.println();
-        System.out.println(( num1 +" + "+ num2 + " = " + a  ) + "\n" +(num1 +" - "+ num2 + " = " + b) + "\n" +
-        (num1 +" * "+ num2 + " = " + c) +"\n"+ (num1 +" / "+ num2 + " = " + d));
+        int n = input.nextInt();
+        for (int i = 0; i < 10; i++){
+            System.out.println(n + " * " + i + " = " + (n * i));
+        }
     }
 }
