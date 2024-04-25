e3b8af4b9e5158ee30e0d693761a2cfb64d47b62
GermanGric <gric427@gmail.com>
2024-04-25 20:28:26 +0300

    Working with arrays, I created a products file and added a discount to products with a price over 1000 rubles. Test 2

--- StringExperiments/src/Main.java
+++ StringExperiments/src/Main.java
@@ -16,10 +16,19 @@ public class Main {
           new Product("chicken", 250),
           new Product("melon",20),
           new Product("mixer", 1200),
-          new Product("kettle", 1800)
+          new Product("kettle", 1800),
+                new Product("молоко", 65),
+                new Product("meet", 420),
+                new Product("milk",65),
+                new Product("bread",40),
+                new Product("a carrot", 250),
+                new Product("chicken", 250),
+                new Product("melon",20),
+                new Product("mixer", 1200),
+                new Product("kettle", 1800)
         };
         int MIN_PRICE_FOR_DISCOUNT = 1000;
-        double discount = 0.1;
+        double discount = 0.2;
         for (Product product : products){
             int price = product.getPrice();
             if (price >= MIN_PRICE_FOR_DISCOUNT){
