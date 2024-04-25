72392929dba4928658865be4d951f180e2e956ec
GermanGric <gric427@gmail.com>
2024-04-25 19:10:58 +0300

    array certCount random and winnerRate

--- StringExperiments/src/Main.java
+++ StringExperiments/src/Main.java
@@ -8,11 +8,12 @@ public class Main {
 
     public static void main(String[] args) {
         int certCount = 1000;
+        int winnerRate = 100;
         int[] certNumbers = new int[certCount];
+        boolean[] certIsWin = new boolean[certCount];
         for (int i = 0; i < certNumbers.length; i++){
             certNumbers[i] = 1_000_000 + (int) Math.round(8_999_999 * Math.random());
-        }
-        for (int i = 0; i < certNumbers.length; i++){
+            certIsWin[i] = i % winnerRate == 0;
             System.out.println(certNumbers[i]);
         }
     }
